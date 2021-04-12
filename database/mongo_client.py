b915bfaae2c3343c7289864f5a11dd7a9286165c
Zumaad Khan <khanzumaad@gmail.com>
2021-04-12 15:30:48 -0500

    changed directory structure

---
+++ database/mongo_client.py
@@ -0,0 +1,24 @@
+import pymongo
+
+class MongoClient:
+    """ responsible for interacting with mongo """
+    def __init__(self):
+        self.mongo_client = pymongo.MongoClient()
+
+    def insertDataByCity(self, cityName, data):
+        """ Sample mongo insert for new or existing city with geospatial index check """
+
+        db = self.client[cityName]
+        posts = db.posts
+        posts.create_index([('geometry', pymongo.GEOSPHERE)])
+        for point in data:
+            posts.insert_one(point)
+
+    def pullDataInRadius(self, cityName, center, radius):
+        """ Sample geospatial mongo query: radius is in meters """
+
+        db = self.client[cityName]
+        posts = db.posts
+        pulls = [record for record in posts.find(
+            {'geometry': {'$near': {'$geometry': {'type': 'Point', 'coordinates': center}, '$maxDistance': radius}}})]
+        return pulls
