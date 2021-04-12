72296c3553e471b37a35df3dda2e1b2aa551ab56
Zumaad Khan <khanzumaad@gmail.com>
2021-04-12 16:52:14 -0500

    cleaned up api return type

--- backend/api.py
+++ backend/api.py
@@ -12,31 +12,31 @@ mongo_client = MongoClient()
 app = FastAPI()
 
 
-class LocationRequest(BaseModel):
+class LocationsRequest(BaseModel):
     start_lon: float
     start_lat: float
     radius: int
     preferences: List[str]
 
 
-@app.post("/locations")
-def location_handler(location_request: LocationRequest) -> List[Dict]:
+class Location(BaseModel):
+    tags: Dict
+    geometry: Dict
+
+
+@app.post("/locations", response_model=List[Location])
+def location_handler(locations_request: LocationsRequest) -> List[Location]:
     """
     Returns a list of locations that a user would want to visit given a user's preferences, their starting location,
     and how far they'd like to explore.
-
-    :param location_request:
-    :return: a list of locations that the user would want to visit
     """
-    print(f"location request: {location_request}")
-    start = [location_request.start_lon, location_request.start_lat]
-    query_result = mongo_client.pullDataInRadius("Boston", start, location_request.radius)
-    print(f"query result len is {len(query_result)}")
-    sanitized_results = []
-    for result in query_result:
-        result['_id'] = str(result['_id'])
-        sanitized_results.append(result)
-    return sanitized_results
+    print(f"location request: {locations_request}")
+    start = [locations_request.start_lon, locations_request.start_lat]
+    query_result = mongo_client.pullDataInRadius("Boston", start, locations_request.radius)
+    locations = []
+    for document in query_result:
+        locations.append(Location(tags=document["tags"], geometry=document["geometry"]))
+    return locations
 
 
 if __name__ == "__main__":
